96afe781850ab3d362adbd589f54cb17f156ec28
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-11-07 18:25:14 -0500

    feat: :sparkles: Configurations and Migration

---
+++ Persistence/Data/DbFirstContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data;
+
+public partial class DbFirstContext : DbContext
+{
+    public DbFirstContext()
+    {
+    }
+
+    public DbFirstContext(DbContextOptions<DbFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<PersonType> PersonTypes { get; set; }
+
+    public virtual DbSet<State> States { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) // 2611
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
